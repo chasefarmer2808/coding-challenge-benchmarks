da45d49dd2662eedc94fe0a71110196e92fada80
Farmer <tma5@cdc.gov>
2024-11-28 13:52:10 -0500

    clean up aoc unit tests

--- pkg/aoc/aoc_test.go
+++ pkg/aoc/aoc_test.go
@@ -9,78 +9,103 @@ import (
 	"testing"
 )
 
-type DayCase struct {
-	inputFile string
-	part1     PartFunc
-	part2     PartFunc
-	p1Val     int
-	p2Val     int
+type Day struct {
+	inputFile     string
+	testInputFile string
+	parts         []Part
+}
+
+type Part struct {
+	partFunc        PartFunc
+	expectedVal     int
+	expectedTestVal int
+}
+
+var days = []Day{
+	{
+		inputFile:     "./2023/day01/input.txt",
+		testInputFile: "./2023/day01/input_test.txt",
+		parts: []Part{
+			{
+				partFunc:        day01.Part1,
+				expectedVal:     55816,
+				expectedTestVal: 0,
+			},
+			{
+				partFunc:        day01.Part2,
+				expectedVal:     54980,
+				expectedTestVal: 0,
+			},
+		},
+	},
+	{
+		inputFile:     "./2023/day02/input.txt",
+		testInputFile: "./2023/day02/input_test.txt",
+		parts: []Part{
+			{
+				partFunc:        day02.Part1,
+				expectedVal:     2207,
+				expectedTestVal: 0,
+			},
+			{
+				partFunc:        day02.Part2,
+				expectedVal:     62241,
+				expectedTestVal: 0,
+			},
+		},
+	},
+	{
+		inputFile:     "./2023/day03/input.txt",
+		testInputFile: "./2023/day03/input_test.txt",
+		parts: []Part{
+			{
+				partFunc:        day03.Part1,
+				expectedVal:     539590,
+				expectedTestVal: 0,
+			},
+			{
+				partFunc:        day03.Part2,
+				expectedVal:     80703636,
+				expectedTestVal: 0,
+			},
+		},
+	},
 }
 
 func Test2023(t *testing.T) {
-	cases := []DayCase{
-		{
-			inputFile: "./2023/day01/input.txt",
-			part1:     day01.Part1,
-			part2:     day01.Part2,
-			p1Val:     55816,
-			p2Val:     54980,
-		},
-		{
-			inputFile: "./2023/day02/input.txt",
-			part1:     day02.Part1,
-			part2:     day02.Part2,
-			p1Val:     2207,
-			p2Val:     62241,
-		},
+	for i, d := range days {
+		runDay(d, i+1, t)
 	}
+}
 
-	for _, c := range cases {
-		f, err := os.Open(c.inputFile)
-		if err != nil {
-			t.Errorf("cannot open input file %s; %s", c.inputFile, err.Error())
-		}
-		defer f.Close()
+func Test2023Day01(t *testing.T) {
+	runDay(days[0], 1, t)
+}
 
-		p1 := c.part1(f)
-		if p1 != c.p1Val {
-			t.Errorf("part 1 failed. expected %v but got %v", c.p1Val, p1)
+func Test2023Day02(t *testing.T) {
+	runDay(days[1], 2, t)
+}
+
+func Test2023Day03(t *testing.T) {
+	runDay(days[2], 3, t)
+}
+
+func runDay(d Day, i int, t *testing.T) {
+	f, err := os.Open(d.inputFile)
+	if err != nil {
+		t.Errorf("cannot open input file %s; %s", d.inputFile, err.Error())
+	}
+	defer f.Close()
+
+	for j, p := range d.parts {
+		soln := p.partFunc(f)
+		if soln != p.expectedVal {
+			t.Errorf("failed for day %d part %d; expected %d but got %d", i, j+1, p.expectedVal, soln)
 		}
 
 		_, err = f.Seek(0, io.SeekStart)
 		if err != nil {
 			t.Errorf("failed to rewind file; %s", err.Error())
 		}
-
-		p2 := c.part2(f)
-		if p2 != c.p2Val {
-			t.Errorf("part 2 failed. expected %v but got %v", c.p2Val, p2)
-		}
-	}
-}
-
-func Test2023Day03(t *testing.T) {
-	inputFile := "./2023/day03/input.txt"
-	expectedP1 := 539590
-	expectedP2 := 80703636
-	f, err := os.Open(inputFile)
-	if err != nil {
-		t.Errorf("cannot open input file %s; %s", inputFile, err.Error())
-	}
-	defer f.Close()
-
-	p1 := day03.Part1(f)
-	if p1 != expectedP1 {
-		t.Errorf("part 1 failed. expected %v but got %v", expectedP1, p1)
-	}
-
-	_, err = f.Seek(0, io.SeekStart)
-	if err != nil {
-		t.Errorf("failed to rewind file; %s", err.Error())
-	}
-
-	p2 := day03.Part2(f)
-	if p2 != expectedP2 {
-		t.Errorf("part 2 failed. expected %v but got %v", expectedP2, p2)
 	}
 }
